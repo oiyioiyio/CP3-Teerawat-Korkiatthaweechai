6c2c3833f6eb09dcc5bcf66e9f14e71e9cede229
oiyioiyio <oiyioiyio@hotmail.com>
2021-02-14 20:45:53 +0700

    Create Exercise 4

---
+++ Exercise4_Teerawat_K.py
@@ -0,0 +1,10 @@
+EnglishScore = 80.5
+GeneralBusinessScore = 95.7
+IntroComputerSysScore = 88.6
+ComputerProgrammingScore = 78.5
+
+print("--- Your Score ---")
+print("Foundation English :", EnglishScore)
+print("General Business :", GeneralBusinessScore)
+print("Introduction to Computer Systems :", IntroComputerSysScore)
+print("Computer Programming :", ComputerProgrammingScore)
